244936862438e1c2ea45843abe72aae539473f53
Jaber Fawaz <rubma_777@hotmail.com>
2025-11-04 23:00:48 +0200

    Add /ping health route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
+
+Route::get('/ping', function () {
+    if (!Schema::hasTable('users')) {
+        Artisan::call('migrate', ['--force' => true]);
+    }
+    return "ok, users=" . User::count();
+});
